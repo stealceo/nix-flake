254b5622c918461ab0e79aa3ff1eead9d190222f
stealceo <gh@sneakernet.top>
2023-10-08 22:02:15 +0300

    jck

--- home/gui/hypr/default.nix
+++ home/gui/hypr/default.nix
@@ -15,7 +15,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable
-    enableNvidiaPatches
+    enableNvidiaPatches = true;
     extraConfig = ''
 
 # See https://wiki.hyprland.org/Configuring/Monitors/
